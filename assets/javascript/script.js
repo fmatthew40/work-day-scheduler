650ad3c84dc9e85e5fa04bcd1bc5e87ccc498558
Matt Foster <fostmatt@Matts-MacBook-Pro.local>
2021-11-30 22:40:17 -0700

    add ability to load from localstorage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,4 @@
+
 // displays the current day at the top under title and subheading
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
@@ -7,6 +8,23 @@ changeColor();
 var currentHour = moment().hours();
 
 function changeColor() {
+
+if (moment().hours() > 8) {
+    $("#eight").addClass("past");
+}if (moment().hours() < 8){
+    $("#eight").addClass("future");
+}if (moment().hours() === 8){
+    $("#eight").addClass("present");
+};
+
+if (moment().hours() > 9) {
+    $("#nine").addClass("past");
+}if (moment().hours() < 9){
+    $("#nine").addClass("future");
+}if (moment().hours() === 9){
+    $("#nine").addClass("present");
+ };
+
 if (moment().hours() > 9) {
     $("#nine").addClass("past");
 }if (moment().hours() < 9){
@@ -104,5 +122,71 @@ $(".savebutton").on("click", function(){
     localStorage.setItem(workhour, JSON.stringify(comment));
 });
 
+// load comments and keep them on the page
 
+function loadEight() {
+eightAm.innerText = JSON.parse(localStorage.getItem("8AM"));
+}
+var eightAm = document.getElementById('eight');
+loadEight();
+
+function loadNine() {
+nineAm.innerText = JSON.parse(localStorage.getItem("9AM"));
+}
+var nineAm = document.getElementById('nine');
+loadNine();
+
+function loadTen() {
+tenAm.innerText = JSON.parse(localStorage.getItem("10AM"));
+}
+var tenAm = document.getElementById('ten');
+loadTen();
+
+function loadEleven() {
+elevenAm.innerText = JSON.parse(localStorage.getItem("11AM"));
+}
+var elevenAm = document.getElementById('eleven');
+loadEleven();
+
+function loadTwelve() {
+twelveAm.innerText = JSON.parse(localStorage.getItem("12AM"));
+}
+var twelveAm = document.getElementById('twelve');
+loadTwelve();
+
+function loadOne() {
+onePm.innerText = JSON.parse(localStorage.getItem("1PM"));
+}
+var onePm = document.getElementById('one');
+loadOne();
+
+function loadTwo() {
+twoPm.innerText = JSON.parse(localStorage.getItem("2PM"));
+}
+var twoPm = document.getElementById('two');
+loadTwo();
+
+function loadThree() {
+threePm.innerText = JSON.parse(localStorage.getItem("3PM"));
+}
+var threePm = document.getElementById('three');
+loadThree();
+
+function loadFour() {
+fourPm.innerText = JSON.parse(localStorage.getItem("4PM"));
+}
+var fourPm = document.getElementById('four');
+loadFour();
+
+function loadFive() {
+fivePm.innerText = JSON.parse(localStorage.getItem("5PM"));
+}
+var fivePm = document.getElementById('five');
+loadFive();
+
+function loadSix() {
+sixPm.innerText = JSON.parse(localStorage.getItem("6PM"));
+}
+var sixPm = document.getElementById('six');
+loadSix();
 
